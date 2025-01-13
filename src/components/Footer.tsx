32b5e6571c02694bd4352631a7b5c43187d4e6df
albertchouforces <albert.chou.forces@gmail.com>
2025-01-13 14:01:55 -0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,8 @@ interface FooterProps {
 export function Footer({ className = "" }: FooterProps) {
   return (
     <footer className={`w-full py-4 text-center text-sm text-gray-500 ${className}`}>
-      (Version 1.0) This is a professional development learning project. For more information please contact the Learning Support Centre Product Development Lead (Pacific) at{' '}
+      (Version 1.0)<br></br>
+      This is a professional development learning project. For more information please contact the Learning Support Centre Product Development Lead (Pacific) at{' '}
       <a 
         href="mailto:joshua.hawthorne@ecn.forces.gc.ca"
         className="text-blue-600 hover:text-blue-800 hover:underline transition-colors"
